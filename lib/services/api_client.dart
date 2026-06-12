45c78251899ef7f347c4e916cb49a73c1c54c1ed
Alexandre Ferrer <afermar3@upv.edu.es>
2026-06-12 07:52:08 +0200

    limpieza

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -3,7 +3,7 @@ class ApiClient {
   static const String baseUrl = "http://127.0.0.1:8000";
 
   // Si pruebas en emulador Android, usa esta:
-   //static const String baseUrl = "http://10.0.2.2:8000";
+  //static const String baseUrl = "http://10.0.2.2:8000";
 
   // Si pruebas en móvil físico, usa la IP de tu PC:
   // static const String baseUrl = "http://192.168.X.X:8000";
