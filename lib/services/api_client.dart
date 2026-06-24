4b212bbd1544c88ab59f5c343bf1afbf781e67ba
Alexandre Ferrer <afermar3@upv.edu.es>
2026-06-24 18:59:20 +0200

    parametros movil fisico

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,6 +1,7 @@
 class ApiClient {
   // Para probar en Windows o Chrome
-  static const String baseUrl = "http://127.0.0.1:8000";
+  //static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "http://172.20.10.5:8000";
 
   // Si pruebas en emulador Android, usa esta:
   //static const String baseUrl = "http://10.0.2.2:8000";
